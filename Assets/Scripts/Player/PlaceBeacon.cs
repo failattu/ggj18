ce33b31a142b3497e8090095ece8a31823a750cd
Tomi Toikka <toikka@catland.studio>
2018-01-26 21:43:02 +0200

    Beacon + simple spawn script

---
+++ Assets/Scripts/Player/PlaceBeacon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceBeacon : MonoBehaviour 
+{
+    Ray ray;
+    RaycastHit hit;
+    public LayerMask layers;
+    public GameObject beaconPrefab;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity, layers))
+            {
+                Instantiate(beaconPrefab, hit.point, Quaternion.identity);
+            }
+        }
+    }
+}
