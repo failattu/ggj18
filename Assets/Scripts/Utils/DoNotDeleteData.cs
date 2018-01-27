69ca1c61586da42829291d02539229359da92b44
Kalle Sirkesalo <kalle.sirkesalo@eficode.com>
2018-01-27 20:58:50 +0200

    Added change of scene

---
+++ Assets/Scripts/Utils/DoNotDeleteData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoNotDeleteData : MonoBehaviour 
+{
+	public static DoNotDeleteData Instance;
+
+	void Awake ()   
+	{
+		if (Instance == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			Instance = this;
+		}
+		else if (Instance != this)
+		{
+			Destroy (gameObject);
+		}
+	}
+}
+
---
+++ Assets/Scripts/Utils/bootItUp.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class bootItUp : MonoBehaviour
+{
+	
+	public void sceneChange()
+	{
+		
+		SceneManager.LoadScene("_DevUI kossu");
+	}
+}
+	
